a1b6162faf26fa009de4299482e148eaae1e40fa
Jose Luis Leon <joseluis5000l@gmail.com>
2024-06-30 22:43:44 -0500

    feat(babel): Extension resolver plugin (#28)

--- src/lib/babelRegister.ts
+++ src/lib/babelRegister.ts
@@ -4,23 +4,13 @@ register({
   cache: true,
   only: [/node_modules[/\\](react-native|@react-native)[/\\]/],
   plugins: [
-    ["module-resolver", {
-      resolvePath(sourcePath: string) {
-        if (sourcePath.startsWith(".")) {
-          const hastePaths = [
-            "/Image",
-            "/Platform",
-            "/PlatformColorValueTypes",
-            "/BaseViewConfig",
-          ];
-
-          return hastePaths.some(path => sourcePath.endsWith(path))
-            ? `${sourcePath}.ios`
-            : undefined;
-        }
-
-        return undefined;
-      },
+    ["extension-resolver", {
+      extensions: [
+        ".ios.js",
+        ".ios.jsx",
+        ".js",
+        ".jsx",
+      ],
     }],
   ],
   presets: ["module:@react-native/babel-preset"],
